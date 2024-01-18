78d130f1ecb654840305b478a4e894bce26a2a4e
Gusers Missin <missingusers@gmail.com>
2024-01-19 07:20:29 +0900

    let

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const a = 10;
-const b = 2;
-const myName = "nico";
+let a = 10;
+let b = 2;
+let myName = "nico";
+// const myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 
 console.log("hello " + myName);
+
+myName = "nicolas";
+
+console.log("your new name is " + myName);
