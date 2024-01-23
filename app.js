76d416ec32c8495cc9cd7ea805540fefb7029324
Gusers Missin <missingusers@gmail.com>
2024-01-24 07:23:01 +0900

    parseInt

--- app.js
+++ app.js
@@ -1,39 +1,10 @@
-function sayHello(nameOfPerson, age ){
-    console.log("Hello my name is " + nameOfPerson + "! And I'm " + age + " years old!");
-}
+const age = prompt("How old are you?");
 
-sayHello("nico", 10);
-sayHello("dal", 20);
-sayHello("lynn", 23);
+console.log(parseInt(age))
 
-function plus(firstNumber, secondNumber){
-    console.log(firstNumber + secondNumber);
-}
-
-function divide(firstNumber, secondNumber){
-    console.log(firstNumber / secondNumber);
-}
-
-//without input -> NaN
-plus();
-
-// n / 0 -> Infinity
-divide(100, 0);
-
-plus(8, 60);
-divide(98, 20);
-
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonsName){
-        if(otherPersonsName === undefined){
-            console.log("Hello! My name is " + player.name + "!");
-        } else {
-            console.log("Hello! " + otherPersonsName + "! My name is " + player.name + "!");
-        }
-    },
-};
-
-console.log(player.name);
-player.sayHello();
-player.sayHello("Vanilla");
+/*
+if(parseInt(age) === NaN){
+    console.log("Input correct age");
+}else{
+    console.log("Your age is " + age);
+}*/
