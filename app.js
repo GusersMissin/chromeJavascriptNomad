b96afd97c1c23b8ca54ffe8e987a2c893a09b4d1
Gusers Missin <missingusers@gmail.com>
2024-02-05 06:25:01 +0900

    3.0 The Document Object

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const age = parseInt(prompt("How old are you?"));
-
-if(isNaN(age)){
-    console.log("Input correct age");
-}else{
-    console.log("Your age is " + age);
-}
+//Title set JS > html
+document.title = "Hello from JS"
