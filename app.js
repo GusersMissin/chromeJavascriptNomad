34b00756ae19b4578659417e83c04c577508e971
Gusers Missin <missingusers@gmail.com>
2024-02-28 06:27:43 +0900

    #3.1 HTML in Javascript

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-//Title set JS > html
-document.title = "Hello from JS"
+const title = document.getElementById("title");
+
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
